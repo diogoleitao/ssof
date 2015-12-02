bb52d077f407229ed00bacfbbd2aa3c3f714424c
tmic <tristaoeast@gmail.com>
2015-12-02 23:31:35 +0000

    Edited config.php

--- apps/xvwa-master/config.php
+++ apps/xvwa-master/config.php
@@ -1,9 +1,9 @@
 <?php
-$XVWA_WEBROOT = '';
+$XVWA_WEBROOT = '/var/www/html/xvwa';
 $host = "localhost";
 $dbname = 'xvwa';
 $user = 'root';
-$pass = '';
+$pass = 'root';
 $conn = mysql_connect($host,$user,$pass);
 $conn1 = new mysqli($host, $user, $pass, $dbname);
 ?>
